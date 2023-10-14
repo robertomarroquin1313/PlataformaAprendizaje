cea396c0fbcf38c492c3c10fb5f5cd358f2051db
Edward-xd <cortejo1234@gmail.com>
2023-10-14 17:41:44 -0600

    Se agrego el el apartado de modificar usuario

---
+++ Assets/js/usuario.js
@@ -0,0 +1,16 @@
+const defaultFile = 'https://stonegatesl.com/wp-content/uploads/2021/01/avatar-300x300.jpg';
+
+const file = document.getElementById( 'foto' );
+const img = document.getElementById( 'img' );
+file.addEventListener( 'change', e => {
+  if( e.target.files[0] ){
+    const reader = new FileReader( );
+    reader.onload = function( e ){
+      img.src = e.target.result;
+    }
+    // que la imagen la pase a formato URL
+    reader.readAsDataURL(e.target.files[0])
+  }else{
+    img.src = defaultFile;
+  }
+} );
