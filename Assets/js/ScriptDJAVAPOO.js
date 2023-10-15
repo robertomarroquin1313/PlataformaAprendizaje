37226c52c672efee255cccda228a78a568ae73cc
layrox-afk <samuelisaia384@gmail.com>
2023-10-14 18:14:18 -0600

    en resumen agregue que aguarade si es admin etc en cokie

--- Assets/js/ScriptDJAVAPOO.js
+++ Assets/js/ScriptDJAVAPOO.js
@@ -1,4 +1,4 @@
- document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
         const checkboxButton = document.getElementById("checkboxButton");
         let isChecked = false;
 
@@ -14,4 +14,32 @@
                 checkboxButton.textContent = "Marcar como pendiente";
             }
         });
+ });*/
+    // Obtener el  usuario almacenado en la cookie
+const tipoUsuario = getCookie("tipoUsuario");
+const botonesProfesor = document.querySelectorAll('.botones-profesor');
+if (tipoUsuario === "profesor") {
+    botonesProfesor.forEach(elemento => {
+        elemento.style.display = "block";
     });
+} else {
+    botonesProfesor.forEach(elemento => {
+        elemento.style.display = "none";
+    });
+}
+function getCookie(name) {///aqui se llama la funcion
+    const cookieName = name + "=";//es la parte que queres recuperar de la cookie con el nombre de la cokiee entre todas la cookies
+    const cookies = document.cookie.split(';');//sepera las cookies en cadenas separadas por ; ya que uelen estar junatas
+    for (let i = 0; i < cookies.length; i++) {//recorrera todas la cookies de la pagina 
+        let cookie = cookies[i];// Se crea una variable cookie que almacena el valor de la cookie actual en cada iteración del bucle.
+        while (cookie.charAt(0) === ' ') {//Este bucle while se utiliza para eliminar cualquier espacio en blanco al principio de la cadena de la cookie.
+            cookie = cookie.substring(1);//: Si se encuentra un espacio en blanco al principio de la cookie, se elimina ese espacio 
+        }
+        if (cookie.indexOf(cookieName) === 0) {//se verifica que los nombre sean iguales
+            return cookie.substring(cookieName.length, cookie.length);//si son iguales devueleve el valor de la cookie 
+        }
+    }
+    return "";
+}
+
+ 
