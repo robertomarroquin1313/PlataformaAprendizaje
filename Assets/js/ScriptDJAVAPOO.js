d18f8ff1244295895b544460a0010c23c61d88a6
layrox-afk <samuelisaia384@gmail.com>
2023-10-16 16:20:33 -0600

    puse que al agregar componetes se puda editar el  titurlo y aplicar una fecha de cierre

--- Assets/js/ScriptDJAVAPOO.js
+++ Assets/js/ScriptDJAVAPOO.js
@@ -15,6 +15,17 @@
             }
         });
  });*/
+ const botonAgregar = document.getElementById("agregarFormulario");
+const formularioFlotante = document.getElementById("formularioFlotante");
+
+botonAgregar.addEventListener("click", function() {
+    if (formularioFlotante.style.display === "block") {
+        formularioFlotante.style.display = "none";
+    } else {
+        formularioFlotante.style.display = "block";
+    }
+});
+
     // Obtener el  usuario almacenado en la cookie
 const tipoUsuario = getCookie("tipoUsuario");
 const botonesProfesor = document.querySelectorAll('.botones-profesor');
@@ -43,6 +54,310 @@ function getCookie(name) {///aqui se llama la funcion
 
     
 }////////////////////////////////////////
+
+$(document).ready(function () {
+    var formularioVisible = false; 
+    var componenteVisible = false; 
+
+    $("#agregarFormulario").click(function () {
+        formularioVisible = !formularioVisible;
+        if (formularioVisible) {
+            $("#formularioFlotante").show();
+            $("#tituloEditable, #fechaEntrega, #guardarCambios, #txt").hide();
+            componenteVisible = false;
+        } else {
+            $("#formularioFlotante").hide();
+        }
+    });
+
+    $("#foro, #videos, #examen").click(function () {
+        if (!componenteVisible) {
+            componenteVisible = true;
+            $("#tituloEditable, #fechaEntrega, #guardarCambios, #txt").show();
+        }
+
+        var botonPresionado = this.id; 
+        $("#guardarCambios").click(function () {
+            var titulo = $("#tituloEditable").val();
+            var fechaEntrega = $("#fechaEntrega").val();
+            var nuevoComponente = "";
+
+            if (botonPresionado === "foro") {
+                nuevoComponente = `
+                
+<div class="botones-profesor text-center toggle-buttons">
+    <button class="btn btn-primary" onclick="ocultarElemento2(this)">
+        <i class="far fa-eye"></i> Ocultar
+    </button>
+    <button class="btn btn-danger" onclick="eliminarElemento2(this)">
+        <i class="fas fa-trash-alt"></i> Eliminar
+    </button>
+    <button class="btn btn-success" onclick="modificarElemento2(this)">
+        <i class="fas fa-pencil-alt"></i> Modificar
+    </button>
+</div>
+                                
+<div class="container color-div">
+    <div class="container img-large ">
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                <img src="/Assets/img/foro.png" alt="Tu Imagen" class="img-fluid img-large">
+            </div>
+        </div>
+    </div>
+<div class="componente foro">
+                        <h3 contenteditable="true">${titulo}</h3>
+                        <p>Fecha de entrega: ${fechaEntrega}</p>
+                    </div>
+
+
+
+    <div class="white-div mb-4 d-flex justify-content-between align-items-center">
+        
+    <a href="/Assets/Pages/PaginasCursoPoo/foro.html" class="btn btn-primary">
+        <i class="fas fa-comments"></i> Foro
+    </a>
+
+
+        <button id="checkboxButton" class="btn btn-checkbox">
+            Marcar como hecho <i class="fas fa-check-circle"></i>
+        </button>
+    </div>
+                `;
+            } else if (botonPresionado === "videos") {
+                nuevoComponente = `
+               
+                  <div class="botones-profesor text-center toggle-buttons">
+        <button class="btn btn-primary" onclick="ocultarElemento2(this)">
+            <i class="far fa-eye"></i> Ocultar
+        </button>
+        <button class="btn btn-danger" onclick="eliminarElemento2(this)">
+            <i class="fas fa-trash-alt"></i> Eliminar
+        </button>
+        <button class="btn btn-success" onclick="modificarElemento2(this)">
+            <i class="fas fa-pencil-alt"></i> Modificar
+        </button>
+    </div>
+
+
+<div class="componente videos">
+                        <h3 contenteditable="true">${titulo}</h3>
+                        <p>Fecha de entrega: ${fechaEntrega}</p>
+                    </div>
+    <div class="container color-div">
+        <div class="container img-large ">
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                    <img src="/Assets/img/Actividad sumativa.png" alt="Tu Imagen" class="img-fluid img-large">
+                </div>
+            </div>
+        </div>
+        <div class="white-div mb-4 d-flex justify-content-between align-items-center">
+    
+       <div class="container text-center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/tcza2FEz4u4?si=lLvymj0MpA3diKC0"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+       </div>
+            
+        </div>
+                
+                    
+                `;
+            } else if (botonPresionado === "examen") {
+                nuevoComponente = `
+                    
+                    <div class="botones-profesor text-center toggle-buttons" >
+        <button class="btn btn-primary" onclick="ocultarElemento2(this)">
+            <i class="far fa-eye"></i> Ocultar
+        </button>
+        <button class="btn btn-danger" onclick="eliminarElemento2(this)">
+            <i class="fas fa-trash-alt"></i> Eliminar
+        </button>
+        <button class="btn btn-success" onclick="modificarElemento2(this)">
+            <i class="fas fa-pencil-alt"></i> Modificar
+        </button>
+    </div>
+            <div class="container color-div">
+                <div class="container img-large ">
+                    <div class="row justify-content-center mb-4">
+                        <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                            <img src="/Assets/img/Actividad sumativa.png" alt="Tu Imagen" class="img-fluid img-large">
+                        </div>
+                    </div>
+                </div>
+                <div class=" container text-center">
+                <div class="componente examen">
+                        <h3 contenteditable="true" style="color:red;">${titulo}</h3>
+                        <p>Fecha de cierre: ${fechaEntrega}</p>
+                    </div>
+                   
+
+                </div>
+                <div class="white-div mb-4 d-flex justify-content-between align-items-center">
+                    <a href="/Assets/Pages/PaginasCursoPoo/examenJava.html" class="btn btn-primary">
+                        <i class="fas fa-file-alt"></i> Examen
+                    </a>
+                        
+                        
+                        <button id="checkboxButton" class="btn btn-checkbox">
+                            Marcar como hecho <i class="fas fa-check-circle"></i>
+                        </button>
+            
+                   
+                    </div>
+            
+                </div>
+                `;
+            }
+
+            $("#contenedorDivs").append(nuevoComponente);
+
+            $("#tituloEditable").val("");
+            $("#fechaEntrega").val("");
+            $("#formularioFlotante").hide();
+            formularioVisible = false;
+            componenteVisible = false;
+        });
+
+        $("#formularioFlotante").on("reset", function () {
+            $("#tituloEditable").val("");
+            $("#fechaEntrega").val("");
+            $("#tituloEditable, #fechaEntrega, #guardarCambios").hide();
+            componenteVisible = false;
+        });
+    });
+});
+
+/*
+$(document).ready(function () {
+    $("#agregarFormulario").click(function () {
+        $("#formularioFlotante").toggle();
+    });
+
+    // Manejar el evento de clic para cada botón dentro del formulario
+    $("#foro, #videos, #examen, #links").click(function () {
+        var botonPresionado = this.id; // Obtener el ID del botón presionado
+
+        // Mostrar el formulario para crear elementos
+        $("#nuevoComponente").show();
+
+        // Manejar el evento de clic para guardar cambios
+        $("#guardarCambios").click(function () {
+            var titulo = $("#tituloEditable").val();
+            var fechaEntrega = $("#fechaEntrega").val();
+
+            // Crear el nuevo componente según el botón presionado
+            var nuevoComponente = "";
+
+            if (botonPresionado === "foro") {
+                nuevoComponente = `
+                
+<div class="container color-div">
+    <div class="container img-large ">
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                <img src="/Assets/img/foro.png" alt="Tu Imagen" class="img-fluid img-large">
+            </div>
+        </div>
+    </div>
+<div class="botones-profesor text-center toggle-buttons">
+    <button class="btn btn-primary" onclick="ocultarElemento2(this)">
+        <i class="far fa-eye"></i> Ocultar
+    </button>
+    <button class="btn btn-danger" onclick="eliminarElemento2(this)">
+        <i class="fas fa-trash-alt"></i> Eliminar
+    </button>
+    <button class="btn btn-success" onclick="modificarElemento2(this)">
+        <i class="fas fa-pencil-alt"></i> Modificar
+    </button>
+</div>
+<div class="componente foro">
+                        <h3 contenteditable="true">${titulo}</h3>
+                        <p>Fecha de entrega: ${fechaEntrega}</p>
+                    </div>
+
+
+
+    <div class="white-div mb-4 d-flex justify-content-between align-items-center">
+        
+    <a href="/Assets/Pages/PaginasCursoPoo/foro.html" class="btn btn-primary">
+        <i class="fas fa-comments"></i> Foro
+    </a>
+
+
+        <button id="checkboxButton" class="btn btn-checkbox">
+            Marcar como hecho <i class="fas fa-check-circle"></i>
+        </button>
+    </div>
+                    
+                `;
+            } else if (botonPresionado === "videos") {
+                nuevoComponente = `
+                  <div class="botones-profesor text-center toggle-buttons">
+        <button class="btn btn-primary" onclick="ocultarElemento2(this)">
+            <i class="far fa-eye"></i> Ocultar
+        </button>
+        <button class="btn btn-danger" onclick="eliminarElemento2(this)">
+            <i class="fas fa-trash-alt"></i> Eliminar
+        </button>
+        <button class="btn btn-success" onclick="modificarElemento2(this)">
+            <i class="fas fa-pencil-alt"></i> Modificar
+        </button>
+    </div>
+
+
+<div class="componente videos">
+                        <h3 contenteditable="true">${titulo}</h3>
+                        <p>Fecha de entrega: ${fechaEntrega}</p>
+                    </div>
+    <div class="container color-div">
+        <div class="container img-large ">
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                    <img src="/Assets/img/Actividad sumativa.png" alt="Tu Imagen" class="img-fluid img-large">
+                </div>
+            </div>
+        </div>
+        <div class="white-div mb-4 d-flex justify-content-between align-items-center">
+    
+       <div class="container text-center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/tcza2FEz4u4?si=lLvymj0MpA3diKC0"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+       </div>
+            
+        </div>
+                    
+                `;
+            } else if (botonPresionado === "examen") {
+                nuevoComponente = `
+                    <div class="componente examen">
+                        <h3 contenteditable="true">${titulo}</h3>
+                        <p>Fecha de entrega: ${fechaEntrega}</p>
+                    </div>
+                `;
+            }
+
+            // Agregar el nuevo componente al contenedor
+            $("#contenedorDivs").append(nuevoComponente);
+
+            // Restablecer campos y ocultar el formulario
+            $("#tituloEditable").val("");
+            $("#fechaEntrega").val("");
+            $("#nuevoComponente").hide();
+        });
+
+        // Restablecer campos y ocultar el formulario cuando se cancele
+        $("#nuevoComponente").on("reset", function () {
+            $("#tituloEditable").val("");
+            $("#fechaEntrega").val("");
+            $("#nuevoComponente").hide();
+        });
+    });
+});
 
 
 $(document).ready(function () {
@@ -143,9 +458,6 @@ $(document).ready(function () {
             <i class="fas fa-pencil-alt"></i> Modificar
         </button>
     </div>
-
-
-
             <div class="container color-div">
                 <div class="container img-large ">
                     <div class="row justify-content-center mb-4">
@@ -211,4 +523,4 @@ $(document).ready(function () {
         // Agregar el nuevo contenido al contenedor
         $("#contenedorDivs").append(nuevoContenido);
     });
-});
+});*/
