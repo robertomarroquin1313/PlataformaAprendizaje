4c4d43a2260282530f02af251d7b0f9655a73142
Edward-xd <cortejo1234@gmail.com>
2023-10-20 14:06:21 -0600

    Finalizacion del buzon

--- Assets/js/buzon-subirARCH.js
+++ Assets/js/buzon-subirARCH.js
@@ -8,22 +8,68 @@ window.addEventListener("load", () => {
         fileshow(filename, filetype);
     })
 
+
     const fileshow = (filename, filetype) => {
         const showfileboxElem = document.createElement("div");
         showfileboxElem.classList.add("showfilebox");
-        const leftElem= document.createElement("div");
+        const leftElem = document.createElement("div");
         leftElem.classList.add("left");
         const fileTypeElem = document.createElement("span");
         fileTypeElem.classList.add("filetype");
-        fileTypeElem.innerHTML=filetype;
+        fileTypeElem.innerHTML = filetype;
         leftElem.append(fileTypeElem);
-        const filetitleElem= document.createElement("h4");
-        filetitleElem.innerHTML=filename;     
-        leftElem.append(filetitleElem);       
+        const filetitleElem = document.createElement("h4");
+        filetitleElem.innerHTML = filename;
+        leftElem.append(filetitleElem);
         showfileboxElem.append(leftElem);
         const rightElem = document.createElement("div");
         rightElem.classList.add("right");
         showfileboxElem.append(rightElem);
         filewrapper.append(showfileboxElem);
+        document.getElementById('Delete').addEventListener('click', function (e) {
+            filewrapper.removeChild(showfileboxElem);
+        });
+        document.getElementById('delete').addEventListener('click', function (e) {
+            filewrapper.removeChild(showfileboxElem);
+        });
+    }
+})
+
+//Segunda llamada 
+window.addEventListener("load", () => {
+    const input = document.getElementById("upload");
+    const filewrapper = document.getElementById("filewrapper2");
+
+    input.addEventListener("change", (e) => {
+        let filename = e.target.files[0].name;
+        let filetype = e.target.value.split(".").pop();
+        fileshow(filename, filetype);
+    })
+
+
+    const fileshow = (filename, filetype) => {
+        const showfileboxElem = document.createElement("div");
+        showfileboxElem.classList.add("showfilebox");
+        const leftElem = document.createElement("div");
+        leftElem.classList.add("left");
+        const fileTypeElem = document.createElement("span");
+        fileTypeElem.classList.add("filetype");
+        fileTypeElem.innerHTML = filetype;
+        leftElem.append(fileTypeElem);
+        const filetitleElem = document.createElement("h4");
+        filetitleElem.innerHTML = filename;
+        leftElem.append(filetitleElem);
+        showfileboxElem.append(leftElem);
+        const rightElem = document.createElement("div");
+        rightElem.classList.add("right");
+        showfileboxElem.append(rightElem);
+        filewrapper.append(showfileboxElem);
+
+        document.getElementById('delete').addEventListener('click', function (e) {
+            filewrapper.removeChild(showfileboxElem);
+        });
+        document.getElementById('Delete').addEventListener('click', function (e) {
+            filewrapper.removeChild(showfileboxElem);
+        });
     }
 })
