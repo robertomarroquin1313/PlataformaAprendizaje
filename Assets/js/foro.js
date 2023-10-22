736e233fb2418baab74fd57058ff1429945d7f35
Edward-xd <cortejo1234@gmail.com>
2023-10-22 15:56:25 -0600

    Escritura

--- Assets/js/foro.js
+++ Assets/js/foro.js
@@ -5,6 +5,8 @@ class Foro {
     }
 }
 class Agregarforo {
+    //Especificamos 2 constantes. Una para crear elemntos nuevos a partir de los elementos ingresados y otra para mostrar lo
+    // Que se a creado.
     AgregarL(Agregarforo) {
         const productList = document.getElementById('product-list');
         const element = document.createElement('div');
@@ -21,32 +23,37 @@ class Agregarforo {
 
         productList.appendChild(element);
     }
-
+   // Limpiamos los campos del modal emergente
     LimpiarCampos(element) {
         document.getElementById('remover').reset();
     }
+    // Para eliminar hemos creado una "a" Con id de "delete"
+    // Este buscarar ese id y removera su contenido
     EliminarL(element) {
         if (element.name === 'delete') {
             element.parentElement.parentElement.parentElement.remove();
         }
     }
 }
-
+ //Buscaremos el id del boton "Crear foro" con el evento click
 document.getElementById('btnxd').addEventListener('click', function (e) {
     const titulo = document.getElementById('titulo').value;
     const mensaje = document.getElementById('mensaje').value;
     const foro = new Foro(titulo, mensaje);
     const pfc = new Agregarforo();
+    //Llamamos la funcion agregrar
     pfc.AgregarL(foro);
+    // Llamamos la funcion limpiar campos
     pfc.LimpiarCampos();
     remover.reset();
     e.preventDefault();
 });
 
+// funcion para limpiar campos
 function limpiar(){
     titulo.reset();
 }
-
+// Enviamos la informacion creada a "Product-list"
 document.getElementById('product-list').addEventListener('click', function (e) {
     const pfc = new Agregarforo();
     pfc.EliminarL(e.target);
